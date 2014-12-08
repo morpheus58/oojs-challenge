3f2563d1a503226dd3610f8e80ae1b7f4c8d963d
Morya Breland Jr <morya.58@gmail.com>
2014-12-07 23:24:31 -0800

    Final Changes to Prototypal Objects

--- js/shapes.js
+++ js/shapes.js
@@ -13,3 +13,39 @@
     or registerClassicalShape() to register your new shapes with the application. See the
     app.js file for info on these functions.
  */
+'use strict';
+
+
+function createRectangle (left, top, width, height, stylesMap) {
+    var rectangle = createShape(left, top, width, height, stylesMap);
+    rectangle.renderShape = function (canvasCtx) {
+        canvasCtx.fillRect(left, top, width, height);
+    };
+    return rectangle;
+}
+
+registerPrototypalShape('rectangle', createRectangle);
+
+function createCircle (left, top, width, height, stylesMap) {
+    var circle = createShape(left, top, width, height, stylesMap);
+    circle.renderShape = function (canvasCtx) {
+        canvasCtx.beginPath();
+        canvasCtx.arc(left, top, radius, 0, 2*Math.PI);
+        canvasCtx.fill();
+        canvasCtx.closePath();
+    };
+    return circle;
+}
+
+registerPrototypalShape('circle', createCircle);
+
+function createPicture (left, top, width, height, stylesMap) {
+    var picture = createShape(left, top, width, height, stylesMap);
+    picture.renderShape = function (canvasCtx) {
+        var img = document.getElementById("hulk");
+        canvasCtx.drawImage(img, left, top, width, height);
+    };
+    return picture;
+}
+
+registerPrototypalShape('picture', createPicture);
